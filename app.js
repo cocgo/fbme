c74a758e32f17e88a0ce2dc298715072ea2d8bdc
zlz <521401@qq.com>
2018-09-14 14:52:19 +0800

    DUNKSHOT

--- app.js
+++ app.js
@@ -103,8 +103,45 @@ app.post('/webhook', (req, res) => {
       res.sendStatus(404);
     }
   
-  });
+});
 
+
+// 投篮
+app.get('/DUNKSHOT', (req, res) => {
+    let VERIFY_TOKEN = 'DUNKSHOT';
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    if (mode && token) {
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            // res.sendStatus(403);
+            res.status(200).send('DUNKSHOT webhook get...');
+        }
+    }else{
+        res.status(200).send('DUNKSHOT webhook get...');
+    }
+});
+
+app.post('/DUNKSHOT', (req, res) => {  
+     let body = req.body;
+    console.log('DUNKSHOT webhook post:', req.query, req.body);
+    if (body.object === 'page') {
+      body.entry.forEach(function(entry) {
+        let webhook_event = entry.messaging[0];
+        console.log('webhook_event:',webhook_event);
+      });
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      res.sendStatus(404);
+    }
+  
+});
 
 
 http.createServer(app).listen(80);
