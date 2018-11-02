25cddef8a17490495b6a11d24f964c94103452e6
home <521401@qq.com>
2018-11-02 23:51:43 +0800

    test hexists

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
-var schedule = require('node-schedule');
+// var schedule = require('node-schedule');
 
 
-var j = schedule.scheduleJob('30 * * * * *', function(){
-  console.log('every minute');
+// var j = schedule.scheduleJob('30 * * * * *', function(){
+//   console.log('every minute');
+// });
+
+var redis = require("redis");
+var client = redis.createClient();
+client.on("error", function (err) {
+    console.log("Redis Error:" , err);
 });
+client.on('connect', function(){
+    console.log('Redis连接成功.');
+});
+
+// 2243662185705137
+let rehs = client.hexists("FlappyBb", '2243662185705137',null);
+console.log('is have:', rehs);
