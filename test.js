30a7590db608c2401363f720bf6f8726e2129fa9
home <521401@qq.com>
2018-11-02 23:55:46 +0800

    1

--- test.js
+++ test.js
@@ -8,14 +8,20 @@
 var redis = require("redis");
 var client = redis.createClient();
 client.on("error", function (err) {
-    console.log("Redis Error:" , err);
+  console.log("Redis Error:", err);
 });
-client.on('connect', function(){
-    console.log('Redis连接成功.');
+client.on('connect', function () {
+  console.log('Redis连接成功.');
 
-    // 2243662185705137
-let rehs = client.hexists("FlappyBb", '2243662185705137');
-console.log('is have:', rehs);
+  // 2243662185705137
+  let rehs = client.hexists("FlappyBb", '2243662185705137', function (err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('ok:', res);
+    }
+  });
+  console.log('is have:', rehs);
 });
 
 // 2243662185705137
