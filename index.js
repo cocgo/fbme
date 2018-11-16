3176c8c3f6e46850baff593d887646cb3289b1d2
hx <521401@qq.com>
2018-11-16 10:00:52 +0800

    token

--- index.js
+++ index.js
@@ -237,14 +237,14 @@ function callSendAPI(sender_psid, response) {
         },
         "message": response
     }
-
+    
     // EAAHZAzZAClqvUBAEOsBI1OynSrra9T0vVMLI6tZATZCPe6REUfWp5HH7MpMAdhig015I6pKZALQLjEmg9f3b6Aw4wGAvJRCMdcvgqQD5CpDv1ZBHDZArww7KQzK50yafPWACDskrDA3jlgPeS3wkkruhxwaqlgj9rpZArmzgprAL0ZAagG3ytzQG7
     // Send the HTTP request to the Messenger Platform
     
     request({
         "uri": "https://graph.facebook.com/me/messages",
         "qs": {
-            "access_token": "EAAD9YRYAeeEBANJuZBeTrd5hVSVxjfyygRYekzvRyFOYH5vkeluZBvIiyZByjT0g4Ir8dDPa5iZA7N7eNZBB1W69MTsYAziEcpDcKo2vEGMOAJ6e10PkrrvZAJj9FkzoDWsiZCPOXA0Y8J2OrZBqazsBj4D3P0mncdt75vzV1MwSkX3pZCpzE5ggPpkhuKf30j9EZD"
+            "access_token": "EAAD9YRYAeeEBAGBQLfvmVJ4hnS6uJFvUh2IW5hdgJM00d60TJbncNOKB0ZCFUXQWI85Fe2NqLDfFfQPXXSDEoT8YUFWFbeKNlaZBmlNbocvakEv0KXrjm15OBQSRpPAMt06hbztZAXEDBT7LScL4Givxzw4ZApE21TYDyYZCODty8lZAAsZBIhnPTaZBAYRf5jfDJIaHhZCdWpA8yrkiMsgtj"
         },
         "method": "POST",
         "json": request_body
